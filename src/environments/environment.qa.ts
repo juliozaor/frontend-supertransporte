7f28f5e8b0ec29074b8568c64f97ec51c866a784
Jesid Polo <jesing482@gmail.com>
2023-06-05 09:49:16 -0500

    actualizando environment qa

--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  urlBackend: 'http://172.16.8.190:3333',
-  urlBackendVigia: 'http://127.0.0.1:3334',
+  urlBackend: 'http://172.16.3.104:3333',
+  urlBackendVigia: 'http://172.16.3.104:3334',
   llaveCaptcha: '6Ldw7FwmAAAAAJZyFVhvohzUI7mEK4wRPEqpGU9g'};
